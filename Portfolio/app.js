7ff3e078850955e02ed6d31412d3d75b7babdc28
MaqsudjonAshurmatov <134127239+MaqsudjonAshurmatov@users.noreply.github.com>
2023-05-26 09:36:49 +0500

    Add files via upload

---
+++ Portfolio/app.js
@@ -0,0 +1,6 @@
+let follow = document.querySelector('.follow');
+let followers = document.querySelector('.followers-number')
+
+follow.addEventListener('click', function(){
+    followers.innerHTML = '378'
+})
